5849011f676c1a882c7e3b13d3efaeb5c88bbb57
Yoonjiwon <k112751@koreainvestment.com>
2022-06-10 08:26:19 +0900

    Skeleton

--- ProtocolPatternWithTMDB/Scene/MovieMainViewController.swift
+++ ProtocolPatternWithTMDB/Scene/MovieMainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieMainViewController.swift
 //  ProtocolPatternWithTMDB
 //
 //  Created by iMac on 2022/06/10.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class MovieMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
