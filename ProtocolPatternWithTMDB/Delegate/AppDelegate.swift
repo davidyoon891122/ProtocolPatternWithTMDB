227eacd43eb06ced730d71a6af720a8b2c4050f7
Yoonjiwon <k112751@koreainvestment.com>
2022-06-10 08:50:14 +0900

    apply swiftLint on main, UnitTextProject

---
+++ ProtocolPatternWithTMDB/Delegate/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ProtocolPatternWithTMDB
+//
+//  Created by iMac on 2022/06/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        return true
+    }
+}
---
+++ ProtocolPatternWithTMDB/Scene/MovieMainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieMainViewModel.swift
+//  ProtocolPatternWithTMDB
+//
+//  Created by iMac on 2022/06/10.
+//
+
+import RxSwift
+import RxRelay
+
+final class MoviewMainViewModel {
+
+}
